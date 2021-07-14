6338aa59c322e3f63c8ab09cc79a59abbd27c4fc
rjbrennan <rjbrennan@localhost>
2021-07-14 16:53:40 -0500

    Changed pGamma input to a slider

--- src/Importer.java
+++ src/Importer.java
@@ -1,10 +1,19 @@
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Hashtable;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.TitledBorder;
 
 import fileChooser.CustomFileChooser;
 import mapstructs.Map;
@@ -38,17 +47,32 @@ public class Importer {
 		while(!map.inMap(topic)) {	
 			topic = JOptionPane.showInputDialog("That topic did not match any concepts, please try again");
 		}
-		String numDialog = "How many clusters would you like";
 		double pGamma = 0;
 		while(pGamma<=0) {
-			//TODO	Change to a slider from low to high
-			try {
-				pGamma = Double.parseDouble(JOptionPane.showInputDialog(numDialog));
+			JFrame frame = new JFrame();
+			JSlider slider = createSlider(10, 40);
+			JPanel sliderPanel = createSliderPanel(slider, "myMessage");
+			String title = "myTitle";
+			int dialogResponse = JOptionPane.showOptionDialog
+		            (frame,                  // I'm within a JFrame here
+		             sliderPanel,
+		             title,
+		             JOptionPane.OK_CANCEL_OPTION,
+		             JOptionPane.QUESTION_MESSAGE,
+		             null, null, null
+		            );
+			if (JOptionPane.OK_OPTION == dialogResponse) {
+				pGamma = slider.getValue();
 			}
-			catch(NumberFormatException e) {
-				numDialog = "Please enter a positive number";
-			}
+			else
+				pGamma = 0;
+			
+			frame.setVisible(false);
+			frame.dispose();
+			
 		}
+		
+		pGamma = pGamma/10;
 		
 		// FIXME if you write the name of another file, it just does filename..cxl
 		System.out.println("Got this far");
@@ -65,5 +89,28 @@ public class Importer {
 		map.execute(output, pGamma);
 
 	}
+	
+	private static JSlider createSlider(int min, int max) {
+		JSlider slider = new JSlider(min, max);
+		slider.setMajorTickSpacing(10);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.setValue(min);
+		Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+		labelTable.put(min, new JLabel("A Couple"));
+		labelTable.put(max, new JLabel("A Lot"));
+		slider.setLabelTable(labelTable);
+		
+		return slider;
+	}
+	
+	public static JPanel createSliderPanel(final JSlider slider, String label) {
+        final JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.setBorder(new TitledBorder(label));
+        p.setPreferredSize(new Dimension(300, 60));
+        p.add(slider);
+        return p;
+    }
 
 }
